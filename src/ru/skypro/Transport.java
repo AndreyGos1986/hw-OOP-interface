6f9d3fcc2ffed20bfe4ca80962088e14c59cdfdb
Андрей <gosudarev_a@mail.ru>
2022-04-25 21:09:27 +0300

    ООП-2 интерфейсы.

---
+++ src/ru/skypro/Transport.java
@@ -0,0 +1,31 @@
+package ru.skypro;
+
+public class Transport implements TransportInterface {
+    private String modelName;
+    private int wheelsCount;
+
+    public Transport(String modelName, int wheelsCount) {
+        this.modelName = modelName;
+        this.wheelsCount = wheelsCount;
+    }
+
+    public String getModelName() {
+        return modelName;
+    }
+
+    public int getWheelsCount() {
+        return wheelsCount;
+    }
+
+    public void updateTyre() {
+        System.out.println("Меняем покрышку");
+    }
+
+    public void checkEngine() {
+        System.out.println("Проверяем двигатель");
+    }
+
+    public void checkTrailer() {
+        System.out.println("Проверяем прицеп");
+    }
+}
